bdbfc630d288da2bb60b462e02da1e73ac5b90b2
Benjamin <benmma13@gmail.com>
2022-08-18 22:47:27 +0400

    update index.js file

--- src/index.js
+++ src/index.js
@@ -4,34 +4,34 @@ import joke from './joke.js';
 // const jokes = document.querySelector('.joke');
 // jokes.innerHTML = joke();
 
-// const todo = document.querySelector('.todo');
+const todo = document.querySelector('.todo');
 
-// const todoArr = [
-//   {
-//     Description: 'ironing',
-//     Completed: 'false',
-//     Index: 0,
-//   },
-//   {
-//     Description: 'washing',
-//     Completed: 'false',
-//     Index: 1,
-//   },
-//   {
-//     Description: 'cooking',
-//     Completed: 'false',
-//     Index: 2,
-//   },
-// ]
+const todoArr = [
+  {
+    Description: 'ironing',
+    Completed: 'false',
+    Index: 0,
+  },
+  {
+    Description: 'washing',
+    Completed: 'false',
+    Index: 1,
+  },
+  {
+    Description: 'cooking',
+    Completed: 'false',
+    Index: 2,
+  },
+]
 
-// function showTask() {
-//   for(let i = 0; i < todoArr.length; i+=1) {
-//     todo.innerHTML += ` <div class="tasks">
-//     <input class="check" type="checkbox">
-//     <p class="txt">${todoArr[i].Description}</p>
-//     <button class="remove-btn">Remove</button>
-//   </div> `;
-//   }
-// }
+function showTask() {
+  for(let i = 0; i < todoArr.length; i+=1) {
+    todo.innerHTML += ` <div class="tasks">
+    <input class="check" type="checkbox">
+    <p class="txt">${todoArr[i].Description}</p>
+    <button class="remove-btn">Remove</button>
+  </div> `;
+  }
+}
 
-// showTask();
+showTask();
